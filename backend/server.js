7f495d1c899108b8a39543f5cc6fd907bb606395
hackhub817 <piyushguptaji123@gmail.com>
2024-10-29 18:35:47 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://project-management-app-front.vercel.app",
     credentials: true,
   })
 );
